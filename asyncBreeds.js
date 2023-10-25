7a6e01d82728e0d35513e8aeee33e990458af5b0
Urvish Prajapati <urvishprajapati412@gmail.com>
2023-10-24 22:29:55 -0400

    cat added

---
+++ asyncBreeds.js
@@ -0,0 +1,19 @@
+// asyncBreeds.js
+const fs = require('fs');
+
+const breedDetailsFromFile = function(breed, functionToRunWhenThingsAreDone) {
+  console.log('breedDetailsFromFile: Calling readFile...');
+  fs.readFile(`./data/${breed}.txt`, 'utf8', (error, data) => {
+    console.log("In readFile's Callback: it has the data.");
+
+    if (!error) functionToRunWhenThingsAreDone(data);
+  });
+
+};
+
+// we try to get the return value
+const printOutCatBreed = (breed) => {
+  console.log('Return Value: ', breed);
+}
+
+breedDetailsFromFile('Bombay', printOutCatBreed);
